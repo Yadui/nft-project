fde66f4d7c7c607d14c295e14a0955f4f067a635
Yadui <84956625+Yadui@users.noreply.github.com>
2022-08-15 10:50:30 +0530

    added required files

---
+++ constants/assets.js
@@ -0,0 +1,53 @@
+import badge from "../assets/icons/badge.png";
+import eth from "../assets/icons/eth.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import nft01 from "../assets/images/nft01.png";
+import nft02 from "../assets/images/nft02.jpeg";
+import nft03 from "../assets/images/nft03.jpeg";
+import nft04 from "../assets/images/nft04.jpeg";
+import nft05 from "../assets/images/nft05.jpeg";
+import nft06 from "../assets/images/nft06.jpeg";
+import nft07 from "../assets/images/nft07.jpeg";
+
+import person01 from "../assets/images/person01.png";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person04.png";
+import person05 from "../assets/images/person05.png";
+import person06 from "../assets/images/person06.png";
+import person07 from "../assets/images/person07.png";
+import person08 from "../assets/images/person08.png";
+import person09 from "../assets/images/person09.png";
+import person10 from "../assets/images/person10.png";
+
+export default {
+  badge,
+  eth,
+  heart,
+  left,
+  search,
+
+  logo,
+  nft01,
+  nft02,
+  nft03,
+  nft04,
+  nft05,
+  nft06,
+  nft07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+  person05,
+  person06,
+  person07,
+  person08,
+  person09,
+  person10,
+};
---
+++ constants/dummy.js
@@ -0,0 +1,202 @@
+import assets from "./assets";
+
+const NFTData = [
+  {
+    id: "NFT-01",
+    name: "Wavy Art #312",
+    creator: "Haneef Erhard",
+    price: 4.25,
+    description:
+      " Such art convey the feelings of comfort and ease, as well as sensual quality as they remind us of the human body.",
+    image: assets.nft01,
+    bids: [
+      {
+        id: "BID-11",
+        name: "Sergio Perez",
+        price: 4.25,
+        image: assets.person02,
+        date: "August 9th, 2018 at 12:10 PM",
+      },
+      {
+        id: "BID-12",
+        name: "Charles Leclerc",
+        price: 4.5,
+        image: assets.person03,
+        date: "September 12th, 2019 at 1:50 PM",
+      },
+      {
+        id: "BID-13",
+        name: "Carlos Ramirez",
+        price: 4.75,
+        image: assets.person04,
+        date: "October 14th, 2019 at 3:50 AM",
+      },
+    ],
+  },
+  {
+    id: "NFT-02",
+    name: "Minimilistic Art, Green",
+    creator: "Dominik Ademar",
+    price: 7.25,
+    description:
+      "Minimalist art is a type of art that deals with simplistic abstract art. Minimalism is a relatively new concept and art style that came into view in the early 1960s.",
+    image: assets.nft02,
+    bids: [
+      {
+        id: "BID-21",
+        name: "Karen Martinez",
+        price: 7.05,
+        image: assets.person05,
+        date: "January 03, 2020 at 01:31 AM",
+      },
+    ],
+  },
+  {
+    id: "NFT-03",
+    name: "Abstract NFT Art",
+    creator: "Picas Orla",
+    price: 95.25,
+    description:
+      "Abstract art is art that does not attempt to represent an accurate depiction of a visual reality but instead use shapes, colours, forms and gestural marks to achieve its effect.",
+    image: assets.nft03,
+    bids: [
+      {
+        id: "BID-31",
+        name: "Julia Murphy",
+        price: 95.25,
+        image: assets.person06,
+        date: "August 01, 2022 at 09:49 AM",
+      },
+      {
+        id: "BID-32",
+        name: "Dennis Ward",
+        price: 95.5,
+        image: assets.person07,
+        date: "July 13, 2022 at 09:01 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-04",
+    name: "Discs NFT",
+    creator: "Hallr Delayahu",
+    price: 54.25,
+    description:
+      "The art remains a vital space to grab attention, contextualize a song, or tell the story of a whole album. It's the image that leads the right people to your music and turns passive listening into active listening. That means turning casual listeners into super-fans.",
+    image: assets.nft04,
+    bids: [
+      {
+        id: "BID-41",
+        name: "Louis Long",
+        price: 56.25,
+        image: assets.person08,
+        date: "September 14, 2021 at 07:11 AM",
+      },
+      {
+        id: "BID-42",
+        name: "Eric Mitchell",
+        price: 54.25,
+        image: assets.person09,
+        date: "July 05, 2021 at 03:59 PM",
+      },
+      {
+        id: "BID-43",
+        name: "Nicholas Sanchez",
+        price: 55.15,
+        image: assets.person02,
+        date: "April 30, 2019 at 3:50 PM",
+      },
+      {
+        id: "BID-44",
+        name: "Deborah Johnson",
+        price: 54.15,
+        image: assets.person04,
+        date: "May 15, 2019 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-05",
+    name: "Colorful sphere art",
+    creator: "Sunny Junius",
+    price: 10.25,
+    description:
+      "This project is a design of a spherical art gallery in the heart of the City within the concept of pure geometry. The sphere was taken as a basis of a pure form from mathematics and the art gallery evolved from the understanding of Leonardo Da Vinci’s Vitruvian Man and",
+    image: assets.nft05,
+    bids: [
+      {
+        id: "BID-51",
+        name: "Muriel Hunter",
+        price: 10.25,
+        image: assets.person01,
+        date: "Febuary 23, 2019 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-06",
+    name: "Black box model",
+    creator: "Vinci",
+    price: 20.25,
+    description:
+      "Relating to performance art, the black box is the name for a square room painted black in which artists performed experimental work. Aldo Tambellini Black Zero. The black box became popular in the late 1960s, when artists began using abandoned warehouses as their studios.",
+    image: assets.nft06,
+    bids: [
+      {
+        id: "BID-61",
+        name: "Wilson Henderson",
+        price: 20.25,
+        image: assets.person05,
+        date: "April 06, 2021 at 09:11 AM",
+      },
+      {
+        id: "BID-62",
+        name: "Samuel Washington",
+        price: 20.5,
+        image: assets.person07,
+        date: "December 27, 2019 at 1:50 PM",
+      },
+      {
+        id: "BID-63",
+        name: "Reginald Carpenter",
+        price: 20.75,
+        image: assets.person03,
+        date: "December 31, 2019 at 3:50 PM",
+      },
+      {
+        id: "BID-64",
+        name: "Darlene Gross",
+        price: 21.25,
+        image: assets.person04,
+        date: "April 01, 2022 at 08:08 AM",
+      },
+      {
+        id: "BID-65",
+        name: "Antoinette Bishop",
+        price: 7.25,
+        image: assets.person01,
+        date: "January 4th at 4:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-07",
+    name: "Abstracto soulful art",
+    creator: "Leonardo",
+    price: 18.25,
+    description:
+      "A soul painting is a specially commissioned artwork just for you. It’s so much more than just a decoration for your home. A soul painting reflects an individual’s essence; it captures the very best we have to offer in definite lines, shapes and colours.",
+    image: assets.nft07,
+    bids: [
+      {
+        id: "BID-65",
+        name: "Rosie Terry",
+        price: 7.25,
+        image: assets.person10,
+        date: "January 4th at 4:50 PM",
+      },
+    ],
+  },
+];
+
+export { NFTData };
